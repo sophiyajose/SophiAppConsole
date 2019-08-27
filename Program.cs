f047dfd5cbd647dbaef0784fcf244403ee692cb2
sophiyajose <54578308+sophiyajose@users.noreply.github.com>
2019-08-27 23:34:23 +0400

    Console Application

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SophiAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal value1;
+            decimal value2;
+            decimal result = 0;
+            string operation;
+            Console.WriteLine("Enter first number: ");
+            value1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            value2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Select operation: ");
+            Console.WriteLine(" Addition(+)");
+            Console.WriteLine(" Subtration(-)");
+            Console.WriteLine(" Multiplication(*)");
+            Console.WriteLine(" Division(/)");
+
+            operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "/":
+                    if (value2 != 0)
+                    {
+                        result = value1 / value2;
+
+                    }
+                    break;
+            }
+            if (value2 == 0 && operation == "/")
+            {
+                Console.WriteLine("Not a number");
+            }
+            else
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+        }
+
+    }
+}
